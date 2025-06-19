e040f142389130819b114c84b68ebdd0de6c7b7e
Dj <177370576+DJS4gnx@users.noreply.github.com>
2025-06-18 22:03:15 -0300

    setup
    im so sorry pocha
    
    Co-Authored-By: pocha <mercworld@pocha.moe>

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/mp-media-players/'
+  base: 'https://djs4gnx.github.io/mp-media-players/',
   plugins: [react()],
 })
