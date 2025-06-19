8583eab1570649059dc9e74ed8033a762bed6c30
Dj <177370576+DJS4gnx@users.noreply.github.com>
2025-06-18 21:42:17 -0300

    we dont need this either

--- codedeploy/hooks/ApplicationStart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euox pipefail
-
-cd /var/lib/codedeploy-apps/media-players
-
-if ! docker compose up --detach --wait --wait-timeout 120 ; then
-    docker compose logs
-    exit 1
-fi
--- codedeploy/hooks/ApplicationStop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -euox pipefail
-
-cd /var/lib/codedeploy-apps/media-players
-
-docker compose down
